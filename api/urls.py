eb81eea01784d207084568768ee8de58b1216c35
CchOOlak <xhamidmozafaryx@gmail.com>
2022-12-21 03:46:06 -0800

    authentication completed

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-     path('hello_world/', views.helloworld)
+     path('hello_world/', views.helloworld),
+     path('auth/register/', views.register),
+     path('auth/login/', views.login),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,62 @@
-from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK
+)
+
 
 # Create your views here.
 
 
+@api_view(["GET"])
 def helloworld(request):
-    return HttpResponse("Hello World!")
+    return Response("Hello World!", status=HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(["POST"])
+def register(request):
+    username = request.data.get("username", None)
+    email = request.data.get("email", None)
+    password = request.data.get("password", None)
+    if username is None or password is None:
+        return Response({'error': 'provide both username and password'},
+                        status=HTTP_400_BAD_REQUEST)
+    try:
+        user = User.objects.create_user(username, email, password)
+        message = "user registered successfully"
+    except:
+        message = "user already exists"
+    user = authenticate(username=username, password=password)
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({
+        'token': token.key,
+        'message': message,
+        }, status=HTTP_200_OK)
+
+@csrf_exempt
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username", None)
+    password = request.data.get("password", None)
+    if username is None or password is None:
+        return Response({'error': 'provide both username and password'},
+                        status=HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if user is None:
+        return Response({
+            'error': 'username or password is wrong',
+            }, status=HTTP_404_NOT_FOUND)
+    
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({
+        'token': token.key,
+        'message': 'login successfully',
+        }, status=HTTP_200_OK)
